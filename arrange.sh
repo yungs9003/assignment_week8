0a3056247c5c6434e73e2dbd89a463dbc816fe91
yungs <yungs9003@naver.com>
2023-04-20 20:20:13 +0900

    i do all

---
+++ arrange.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+cd files
+mv a*.txt ../a
+mv A*.txt ../a
+mv b*.txt ../b
+mv B*.txt ../b
+mv c*.txt ../c
+mv C*.txt ../c
+mv d*.txt ../d
+mv D*.txt ../d
+mv e*.txt ../e
+mv E*.txt ../e
+mv f*.txt ../f
+mv F*.txt ../f
+mv g*.txt ../g
+mv G*.txt ../g
+mv h*.txt ../h
+mv H*.txt ../h
+mv i*.txt ../i
+mv I*.txt ../i
+mv j*.txt ../j
+mv J*.txt ../j
+mv k*.txt ../k
+mv K*.txt ../k
+mv l*.txt ../l
+mv L*.txt ../l
+mv m*.txt ../m
+mv M*.txt ../m
+mv n*.txt ../n
+mv N*.txt ../n
+mv o*.txt ../o
+mv O*.txt ../o
+mv p*.txt ../p
+mv P*.txt ../p
+mv q*.txt ../q
+mv Q*.txt ../q
+mv r*.txt ../r
+mv R*.txt ../r
+mv s*.txt ../s
+mv S*.txt ../s
+mv t*.txt ../t
+mv T*.txt ../t
+mv u*.txt ../u
+mv U*.txt ../u
+mv v*.txt ../v
+mv V*.txt ../v
+mv w*.txt ../w
+mv W*.txt ../w
+mv x*.txt ../x
+mv X*.txt ../x
+mv y*.txt ../y
+mv Y*.txt ../y
+mv z*.txt ../z
+mv Z*.txt ../z
+
+
